f923ff5d71049acea1afc39ece92871a1b22c8d1
Max Stepanov <frermax@yandex.ru>
2023-04-12 15:57:28 +0200

    cats charity fund

--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_title: str
-    app_descr: str
+    app_title: str = 'Cat Charity Fund'
+    app_descr: str = 'Helping cats to live a life'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'secret'
 
